1b9ef452ca415323de5717a2e56f9202dc394c52
zahid <zahidthoker5@gmail.com>
2025-09-26 13:08:27 +0530

    my first commit

---
+++ mcq-ranking-website/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+function App() {
+  return (
+    <div className=" min-h-screen flex flex-col items-center justify-center">
+      <h1 className="flex flex-col justify-center bg-gray-600 text-5xl text-white-600">Welcome to React!</h1>
+    </div>
+  )
+}
+
+export default App
